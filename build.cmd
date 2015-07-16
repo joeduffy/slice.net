361ea071e5128777a0e4f31f7ad43fee877c72b8
Joe Duffy <joeduffy@acm.org>
2015-07-16 16:35:18 -0700

    Build tests w/ debugging symbols

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 mkdir bin
 ilasm /dll src\PtrUtils.il /out:bin\System.Slice.netmodule
 csc /t:library /debug /unsafe /o+ /addmodule:bin\System.Slice.netmodule /out:bin\System.Slice.dll src\*.cs
-csc /unsafe /r:bin\System.Slice.dll /out:bin\System.Slice.Test.exe tests\*.cs
+csc /debug /unsafe /r:bin\System.Slice.dll /out:bin\System.Slice.Test.exe tests\*.cs
+
