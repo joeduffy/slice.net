b4b5a70b0ac3cdb7b907388c137839472d7fee3c
Adam Sitnik <adam.sitnik@powel.com>
2015-07-19 18:46:15 +0200

    build in Release for perf testing

--- build.cmd
+++ build.cmd
@@ -1,5 +1,11 @@
 mkdir bin
-ilasm /dll src\PtrUtils.il /out:bin\System.Slice.netmodule
-csc /t:library /debug /unsafe /o+ /addmodule:bin\System.Slice.netmodule /out:bin\System.Slice.dll src\*.cs
-csc /debug /unsafe /r:bin\System.Slice.dll /out:bin\System.Slice.Test.exe tests\*.cs
 
+mkdir bin\Debug
+ilasm /dll src\PtrUtils.il /out:bin\Debug\System.Slice.netmodule
+csc /t:library /debug /unsafe /o+ /addmodule:bin\System.Slice.netmodule /out:bin\Debug\System.Slice.dll src\*.cs
+csc /debug /unsafe /r:bin\Debug\System.Slice.dll /out:bin\Debug\System.Slice.Test.exe tests\*.cs
+
+mkdir bin\Release
+ilasm /dll src\PtrUtils.il /out:bin\Release\System.Slice.netmodule
+csc /t:library /unsafe /o+ /addmodule:bin\System.Slice.netmodule /out:bin\Release\System.Slice.dll src\*.cs
+csc /unsafe /r:bin\Release\System.Slice.dll /out:bin\Release\System.Slice.Test.exe tests\*.cs
