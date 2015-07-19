63710add2734cae5583447ab9b7209a0dabec308
Adam Sitnik <adam.sitnik@powel.com>
2015-07-19 21:50:19 +0200

    use /debug:pdbonly as suggested to get benefits of pdb files in optimized dll

--- build.cmd
+++ build.cmd
@@ -1,11 +1,4 @@
 mkdir bin
-
-mkdir bin\Debug
-ilasm /dll src\PtrUtils.il /out:bin\Debug\System.Slice.netmodule
-csc /t:library /debug /unsafe /o+ /addmodule:bin\System.Slice.netmodule /out:bin\Debug\System.Slice.dll src\*.cs
-csc /debug /unsafe /r:bin\Debug\System.Slice.dll /out:bin\Debug\System.Slice.Test.exe tests\*.cs
-
-mkdir bin\Release
-ilasm /dll src\PtrUtils.il /out:bin\Release\System.Slice.netmodule
-csc /t:library /unsafe /o+ /addmodule:bin\System.Slice.netmodule /out:bin\Release\System.Slice.dll src\*.cs
-csc /unsafe /r:bin\Release\System.Slice.dll /out:bin\Release\System.Slice.Test.exe tests\*.cs
+ilasm /dll src\PtrUtils.il /out:bin\System.Slice.netmodule
+csc /t:library /debug:pdbonly /unsafe /o+ /addmodule:bin\System.Slice.netmodule /out:bin\System.Slice.dll src\*.cs
+csc /debug:pdbonly /unsafe /r:bin\System.Slice.dll /out:bin\System.Slice.Test.exe tests\*.cs
