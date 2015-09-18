c728c91227436f09b6f921d7922500777453dfcc
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2015-09-18 08:54:46 +0100

    Use link to create a single dll.
    Gets rid of the netmodule, specifically.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,6 @@
 mkdir bin
-ilasm /dll src\PtrUtils.il /out:bin\System.Slice.netmodule
-csc /t:library /debug:pdbonly /unsafe /o+ /addmodule:bin\System.Slice.netmodule /out:bin\System.Slice.dll src\*.cs
+ilasm /dll src\PtrUtils.il /out:bin\System.Slice.Core.netmodule
+csc /t:module /debug:pdbonly /unsafe /o+ /addmodule:bin\System.Slice.Core.netmodule /out:bin\System.Slice.netmodule src\*.cs 
+link /dll /out:bin\System.Slice.dll bin\System.Slice.Core.netmodule bin\System.Slice.netmodule 
 csc /debug:pdbonly /unsafe /r:bin\System.Slice.dll /out:bin\System.Slice.Test.exe tests\*.cs
 
